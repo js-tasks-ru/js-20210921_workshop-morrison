4e3308d318729c60b9b97f02c1b254abed4533ee
ws-morrison <workshop.morrison@gmail.com>
2021-10-01 15:20:17 +0300

    add uniq, invertObj

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,9 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+    const invertedMap = new Map();
+    for (const [obj1, obj2] of Object.entries(obj)) {
+        invertedMap.set(obj2, obj1);
+    }
+    return Object.fromEntries(invertedMap);
 }
