005e456aa15ecb72d0c507210a91ac2cc5129c40
ws-morrison <workshop.morrison@gmail.com>
2021-10-01 23:44:15 +0300

    add trimSymbols, change create-getter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -3,19 +3,22 @@
  * @param {string} path - the strings path separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
-
-export const createGetter = path => {
+export function createGetter(path) {
     const pathArray = path.split('.');
 
     return obj => {
         let result = obj;
 
-        for (const item of pathArray) {
-            if (result === undefined) break;
+        const getValue = index => {
+            if (index === pathArray.length || result === undefined) {
+                return result;
+            }
 
-            result = result[item];
-        }
+            result = result[pathArray[index]];
 
-        return result;
+            return getValue(index + 1);
+        };
+
+        return getValue(0);
     };
-};
+}
--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,19 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+    if (size === 0) return '';
+    if (size === undefined) return string;
 
+    const firstSlice = string.slice(0, size);
+
+    const concatValue = (result, index) => {
+        if (index > string.length - 1) return result;
+
+        const currentChart = string[index];
+        const nextChar = !result.endsWith(currentChart.repeat(size)) ? currentChart : '';
+
+        return concatValue(result + nextChar, index + 1);
+    };
+
+    return concatValue(firstSlice, size);
 }
